1f18ef2b03b694a5fff45295cb48bfedeb88b039
Rahul <rahulna@thoughtworks.com>
2016-10-13 11:57:35 +0530

    Throwing an exception when setOfState does not contain any of the final state

--- src/main/DFA.java
+++ src/main/DFA.java
@@ -7,25 +7,28 @@ import main.components.TransitionTable;
 import java.util.List;
 import java.util.Set;
 
-class FiniteStateMachine {
+class DFA {
     private final TransitionTable transitionTable;
     private final State startingState;
     private Set<State> finalStates;
 
-    public static FiniteStateMachine build(Set<State> setOfStates,
-                                           Set<Alphabet> setOfAlphabet,
-                                           State startingState,
-                                           TransitionTable transitionTable,
-                                           Set<State> finalStates) throws InvalidMachineDefinition {
+    public static DFA build(Set<State> setOfStates,
+                            Set<Alphabet> setOfAlphabet,
+                            State startingState,
+                            TransitionTable transitionTable,
+                            Set<State> finalStates) throws InvalidMachineDefinition {
 
-        if(!transitionTable.isValidFor(setOfStates, setOfAlphabet))
+        if(!transitionTable.isValidFor(setOfStates, setOfAlphabet)) {
             throw new InvalidMachineDefinition("Transition table does not contains all the transitions required for the given tuple");
-        return new FiniteStateMachine(startingState, transitionTable, finalStates);
+        } else if(!setOfStates.containsAll(finalStates)){
+            throw new InvalidMachineDefinition("Set of states does not contains one or more final state");
+        }
+        return new DFA(startingState, transitionTable, finalStates);
     }
 
-    private FiniteStateMachine(State startingState,
-                               TransitionTable transitionTable,
-                               Set<State> finalStates) {
+    private DFA(State startingState,
+                TransitionTable transitionTable,
+                Set<State> finalStates) {
         this.startingState = startingState;
         this.transitionTable = transitionTable;
         this.finalStates = finalStates;
--- test/main/DFATest.java
+++ test/main/DFATest.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class FiniteStateMachineTest {
+public class DFATest {
     @Test
     public void ShouldBeAbleToBuildAFiniteStateMachine() throws Exception {
         State q1 = new State("q1");                     //Initial State
@@ -43,7 +43,7 @@ public class FiniteStateMachineTest {
         HashSet<State> finalStates = new HashSet<>();
         finalStates.add(q2);
 
-        FiniteStateMachine DFA = FiniteStateMachine.build(setOfState, setOfAlphabet, q1, transitionTable, finalStates);
+        DFA DFA = main.DFA.build(setOfState, setOfAlphabet, q1, transitionTable, finalStates);
 
         assertTrue(DFA.isAccepted(Arrays.asList(zero, one, zero, zero)));
         assertFalse(DFA.isAccepted(Arrays.asList(zero, one, zero, zero, one, zero)));
@@ -82,8 +82,41 @@ public class FiniteStateMachineTest {
         thrown.expect(InvalidMachineDefinition.class);
         thrown.expectMessage(CoreMatchers.is("Transition table does not contains all the transitions required for the given tuple"));
 
-        FiniteStateMachine.build(setOfState, setOfAlphabet, q1, transitionTable, finalStates);
+        DFA.build(setOfState, setOfAlphabet, q1, transitionTable, finalStates);
+    }
 
+    @Test
+    public void ShouldThrowExceptionWhenSetOfStatesDoesNotContainsFinalState() throws Exception {
+        State q1 = new State("q1");                     //Initial State
+        State q2 = new State("q2");                     //Final State
+        State q3 = new State("q3");                     //Dead state
+        HashSet<State> setOfState = new HashSet<>();
+        setOfState.add(q1);
+        setOfState.add(q2);
+        setOfState.add(q3);
+
+        Alphabet one = new Alphabet("1");
+        Alphabet zero = new Alphabet("0");
+        HashSet<Alphabet> setOfAlphabet = new HashSet<>();
+        setOfAlphabet.add(one);
+        setOfAlphabet.add(zero);
+
+        TransitionTable transitionTable = new TransitionTable();
+        //Recognizes power of two
+        transitionTable.addTransition(q1, zero, q1);
+        transitionTable.addTransition(q1, one, q2);
+        transitionTable.addTransition(q2, zero, q2);
+        transitionTable.addTransition(q2, one, q3);
+        transitionTable.addTransition(q3, one, q3);
+        transitionTable.addTransition(q3, zero, q3);
+
+        HashSet<State> finalStates = new HashSet<>();
+        finalStates.add(new State("q10"));
+
+        thrown.expect(InvalidMachineDefinition.class);
+        thrown.expectMessage(CoreMatchers.is("Set of states does not contains one or more final state"));
+
+        DFA.build(setOfState, setOfAlphabet, q1, transitionTable, finalStates);
     }
 
 
